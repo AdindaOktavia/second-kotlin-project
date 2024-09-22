f23db15a3301726ddc85265577daac49fc619d4b
Adinda Oktavia <adindaoktaviabks0812@gmail.com>
2024-09-22 21:46:43 +0700

    Add PenerapanFor-LoopPadaTabelPerkalian.kt

--- src/main/kotlin/PenerapanFor-LoopPadaTabelPerkalian.kt
+++ src/main/kotlin/PenerapanFor-LoopPadaTabelPerkalian.kt
@@ -1,46 +1,58 @@
-//Penerapan For-Loop dalam Tabel Perkalian
+// Penerapan For-Loop dalam Tabel Perkalian
 fun main(args: Array<String>) {
     println("Simple multiplication table using loops:")
-    for (i in 1..10){
-        print("$i ")
-    }
-    println()
-    for (i in 1..10){
-        println("${i * 2} ")
-    }
-    println()
-    for (i in 1..10){
-        print("${i * 4} ")
-    }
-    println()
-    for (i in 1..10){
-        print("${i * 5} ")
-    }
-    println()
-    for (i in 1..10){
-        print("${i * 6} ")
-    }
-    println()
+
     for (i in 1..10) {
-        print("${i * 7} ")
+        print("$i\t")  // Tambahkan tab untuk keteraturan
     }
     println()
+
     for (i in 1..10) {
-        print("${i * 8} ")
+        print("${i * 2}\t")
     }
     println()
+
     for (i in 1..10) {
-        print("${i * 9} ")
+        print("${i * 4}\t")
     }
     println()
+
     for (i in 1..10) {
-        print("${i * 10} ")
+        print("${i * 5}\t")
     }
+    println()
+
+    for (i in 1..10) {
+        print("${i * 6}\t")
+    }
+    println()
+
+    for (i in 1..10) {
+        print("${i * 7}\t")
+    }
+    println()
+
+    for (i in 1..10) {
+        print("${i * 8}\t")
+    }
+    println()
+
+    for (i in 1..10) {
+        print("${i * 9}\t")
+    }
+    println()
+
+    for (i in 1..10) {
+        print("${i * 10}\t")
+    }
+
     println("\n\nHere's a simple multiplication table using nested loops:")
-    for (i in 1..10){
-        for (j in 1..10){
-            print("${i * j}")
+
+    // Tabel perkalian menggunakan nested loop
+    for (i in 1..10) {
+        for (j in 1..10) {
+            print("${i * j}\t")  // Gunakan tab untuk keteraturan
         }
-        println()
+        println()  // Pindah ke baris baru setelah setiap baris selesai
     }
 }
